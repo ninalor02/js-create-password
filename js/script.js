e59392e5b78aeb48d6444e616ab61f2ee2339aee
nina <128232339+ninalor02@users.noreply.github.com>
2023-05-17 20:26:42 -0500

    fixed html path link from html to css

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-    console.log("You clicked the button")
+    console.log("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()=+[]-/")
 
-    return "Generated password will go here!"
+
+// prompt user for the password criteria
+// password length 8 < 128
+// lowercase, uppercase, numbers, special characters
+// validate the input
+// display password to the page
+
+    return "password created";
 }
 
 // Write password to the #password input
@@ -12,6 +19,7 @@ function writePassword() {
   var password = generatePassword(5);
   var passwordText = document.querySelector("#password");
 
+  
 
   passwordText.value = password;
 
@@ -19,3 +27,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
