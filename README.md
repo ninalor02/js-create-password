49fd33d06770c60d4e8ab672480e2468d5216d90
nina <128232339+ninalor02@users.noreply.github.com>
2023-05-18 23:05:21 -0500

    fixing bug in JS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+* Create Password / Password Generator
+
+This webpage is created to generate a random password.
+
+The purpose of this generator is suppose to give you a random code.
+
 
--- js/script.js
+++ js/script.js
@@ -7,11 +7,41 @@ function generatePassword(){
 // validate the input
 // display password to the page
 // add prompt /
+//**************************************************//
 
-/* prompts and confirm for the uppercase, lowercase, numbers and symbols */
 
-var lengthPassword = prompt("Choose between 8 and 128 characters in length." );
+
+/* various arrays */
+
+//  single arrays -->  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+console.log(uppercaseArray);
+
+var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+console.log(lowercaseArray);
+
+//you're picking a random element from the array to add to the password and the for loop will need to go as many times as the password is long
+var symbolsArray = ["<", ">","*","/","-","!","@","#","$","%","^","&","=","+"];
+console.log(symbolsArray);
+
+var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
+console.log(numbersArray);
+
+
+/* variable Declaration */
+
+var confirmLength = "";
+var symbols;
+var upperCase;
+var lowerCase;
+var numbers;
+
+/* prompts for password length */
+
+var lengthPassword = prompt("Choose between 8 and 128 characters in length.");
 console.log(lengthPassword);
+
+/* confirm for uppercase, lowercase, numbers and symbols */
 
 var upperCase = confirm("Do you want any UPPERCASE letters?");
 console.log(upperCase);
@@ -25,38 +55,28 @@ console.log(numbers);
 var symbols = confirm("Do you want any symbols?");
 console.log(symbols);
 
-//  single arrays -->  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25
-var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-console.log(uppercaseArray);
-
-var uppercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-console.log(lowercaseArray);
-
-//you're picking a random element from the array to add to the password and the for loop will need to go as many times as the password is long
-var symbolsArray = ["(", ")","*","/","-","!","@","#","$","%","^","&","=","+"];
-console.log(symbolsArray);
-
-var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
-console.log(numbersArray);
-
 //work on figuring out how to get the arrays the user chooses into array (the variable)
 
 
+var generatePassword = [];
+/* confirm uppercase, lowercase, numbers, symbols */
 
 
-if(confirmUpperCase){
-  passwordUpperCase = passwordUpperCase.concat(upperCase)
+if(confirmupperCAse){
+  confirmUppercase = passwordCharacter.concat(upperCase)
 }
 if(confirmLowerCase){
-  passwordLowerCase = passwordLowerCase.concat(lowerCase)
+  confirmLowerCase = passwordCharacter.concat(lowerCase)
 }
 if(confirmNumbers){
-  passwordNumbers = ppasswordNumbers.concat(numbers)
+  confirmNumbers = passwordCharacter.concat(numbers)
 }
 if(confirmSymbols){
-  passwordSymbols = ppasswordSymbols.concat(symbols)
+  confirmSymbols = passwordCharacter.concat(symbols)
 }
+console.log(passwordCharacter)
 
+/* empty string will filled in based on loop randomly selected from array */
 
 var randomPassword = ""
 
@@ -68,11 +88,8 @@ console.log(randomNumbers[i])
   //end of code
 }
 
+return randomPassword;
 
-
-var array = [];
-var password  = ''
-  return password;
 }
 
 // Write password to the #password input
