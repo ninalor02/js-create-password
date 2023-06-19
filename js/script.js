9a72a1cdfdf60cbf07ae1b99ff3842f2e2be56c8
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-19 17:57:03 -0500

    add if and for loop for characters

--- js/script.js
+++ js/script.js
@@ -7,4 +7,30 @@ let confirmNumberChar = false;
 let confirmSymbolChar = false;
 let confirmLength = '';
 
+function generatePassword() {
+  passwordLength();
 
+  let randomPassword = '';
+  let characters = '';
+
+  if(confirmUpperChar === true) {
+      characters += passwordCharacters.upperCaseCharCodes;
+  }
+
+  if(confirmLowerChar === true) {
+      characters += passwordCharacters.lowerCaseCharCodes;
+  }
+
+  if(confirmNumberChar === true) {
+      characters += passwordCharacters.numberCharCodes;
+  }
+
+  if(confirmSymbolChar === true) {
+      characters += passwordCharacters.symbolCharCodes;
+  }
+
+  for(let i = 0; i < confirmLength; i++) {
+      randomPassword += characters.charAt(Math.floor(Math.random() * characters.length));
+  }  
+  return randomPassword;
+}
